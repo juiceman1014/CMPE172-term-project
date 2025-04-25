4dbace7cfebe79e90b0a250ea5821c7727e41ef3
juiceman1014 <justintran1014@gmail.com>
2025-04-24 17:39:19 -0700

    added intermediate queries

--- dml-justin.sql
+++ dml-justin.sql
@@ -1,17 +1,29 @@
---basic query 1
+-- basic query 1
+-- Find suppliers that have been supplying since 2022.
 SELECT Name AS SupplierName
 FROM Supplier
 WHERE DateStartedSupplying <= '2022-12-31';
 
---basic query 2
+-- basic query 2
+-- Find out which advertisements brought in each customer.
 SELECT C.name AS CustomerName, A.description AS AdvertisingDescription
 FROM Customer C, Advertising A
 WHERE C.AdvertisingID = A.AdvertisingID;
 
---intermediate query 1
+-- intermediate query 1
+SELECT C.Name, R.DateOfReview, R.Description, R.Stars
+FROM Customer C
+LEFT JOIN Review R ON C.CustomerID=R.CustomerID
+ORDER BY R.Stars DESC;
 
---intermediate query 2
+-- intermediate query 2
+SELECT M.Name, SUM(O.Quantity) AS TimesSold
+FROM Menu M
+INNER JOIN OrderItem O ON M.MenuItemID=O.MenuItemID
+GROUP BY M.name
+ORDER BY TimesSold DESC;
 
---advanced query 1
+-- advanced query 1
 
---advanced query 2
+
+-- advanced query 2
